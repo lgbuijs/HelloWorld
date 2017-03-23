9ce308fdf805ed9d873ef682fc91b93cdd719853
Louw Buijs <lgbuijs@yahoo.com>
2017-03-23 14:45:31 +0100

    testje

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String args[]){
 		  
 		/* Use System.out.println() to print on console. */
-	   System.out.println("Hello World!");
+	   System.out.println("Hello World - changed!");
 	}
 
 }
