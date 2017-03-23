0fae74202e4c5dd656e781379d33456021d2a7b4
Louw Buijs <lgbuijs@yahoo.com>
2017-03-23 14:41:36 +0100

    Initial creation

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String args[]){
+		  
+		/* Use System.out.println() to print on console. */
+	   System.out.println("Hello World!");
+	}
+
+}
